5ecdd14e431f381f0f27effae462bc80e182c460
krispatl <31904924+krispatl@users.noreply.github.com>
2026-02-25 02:13:05 +0100

    Add files via upload

---
+++ types/esmsh.d.ts
@@ -0,0 +1,6 @@
+declare module "https://esm.sh/*" {
+  // We only rely on Spark's SplatMesh constructor for SPZ.
+  export const SplatMesh: any;
+  const _default: any;
+  export default _default;
+}
